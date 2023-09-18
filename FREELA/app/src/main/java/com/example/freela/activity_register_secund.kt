182b42847919a4499a46a11404c6131724c101d0
romvinicius <vinicius.roman@sptech.school>
2023-09-17 21:57:16 -0300

    Adicionando telas de cadastro

--- FREELA/app/src/main/java/com/example/freela/activity_register_secund.kt
+++ FREELA/app/src/main/java/com/example/freela/activity_register_secund.kt
@@ -3,9 +3,9 @@ package com.example.freela
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class Register : AppCompatActivity() {
+class activity_register_secund : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register_first)
+        setContentView(R.layout.activity_register_secund)
     }
 }
