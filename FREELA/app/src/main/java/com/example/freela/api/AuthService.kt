b375f5ed7470bc1f535b187633335f64c20a4249
Marcos-Henriq <mh7505570@gmail.com>
2023-10-24 13:32:37 -0300

    adiciona rotas de criação de orders

--- FREELA/app/src/main/java/com/example/freela/api/AuthService.kt
+++ FREELA/app/src/main/java/com/example/freela/api/AuthService.kt
@@ -6,15 +6,19 @@ import com.example.freela.model.dto.request.RegisterRequest
 import com.example.freela.model.dto.response.LoginResponse
 import retrofit2.Call
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
 
 interface AuthService {
-    @POST("/user")
+    @POST("/users")
     fun register(@Body registerRequest: RegisterRequest):
             Call<User>
 
-    @POST("/user/login")
+    @POST("/users/authenticate")
     fun login(@Body loginRequest: LoginRequest):
             Call<LoginResponse>
+
+    @GET("/users")
+    fun userDetails(): Call<User>
 
 }
