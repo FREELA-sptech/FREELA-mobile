3e1a9b5e6f9e07a5d8e44087d44ae95f0f6e2773
GuilhermeFGoncalves <guilherme.goncalves@sptech.school>
2023-11-07 22:11:34 -0300

    adiciona o firebase

--- FREELA/app/src/main/java/com/example/freela/view/MainActivity.kt
+++ FREELA/app/src/main/java/com/example/freela/view/MainActivity.kt
@@ -3,9 +3,11 @@ package com.example.freela.view
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import com.example.freela.R
+import com.google.firebase.messaging.FirebaseMessaging
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,6 +16,8 @@ class MainActivity : AppCompatActivity() {
 
         val button : Button = findViewById(R.id.start);
         val redirectLogin : TextView = findViewById(R.id.entrar);
+
+
 
         button.setOnClickListener {
             val intent = Intent(this, Register::class.java)
@@ -24,5 +28,11 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, Login::class.java)
             startActivity(intent)
         }
-    }
+
+            FirebaseMessaging.getInstance().token.addOnCompleteListener { task ->
+                if (task.isSuccessful){
+                    Log.i("token",task.result)
+                }
+            }
+            }
 }
